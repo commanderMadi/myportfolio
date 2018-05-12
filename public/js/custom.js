11c84baed566e988248c0367e493bc2c7ffa0ef7
Darth Coder <00darthcoder00@gmail.com>
2018-05-12 09:17:05 +0200

    Remove hero animation logic

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,57 +4,6 @@
 
 $(function () { //On Document Ready
 
-    /***** Define Class for elements that slide using jQuery animation *****/
-
-    //Constants for left and right position vals.
-    const L = "left";
-    const R = "right";
-
-    class SlidElement {
-        constructor(selector, direction) {
-            this.selector = $(selector);
-            this.direction = direction;
-            this.orient();
-        }
-        lefty() {
-            this.selector.css({
-                opacity: 0.3,
-                position: "relative",
-                left: "50%"
-            });
-            this.animate();
-        }
-        righty() {
-            this.selector.css({
-                opacity: 0.3,
-                position: "relative",
-                right: "50%"
-            });
-            this.animate();
-        }
-
-        animate() {
-            this.selector.animate({
-                opacity: 1,
-                left: '0'
-            }, 3000);
-        }
-        orient() {
-            if (this.direction === "left") {
-                this.lefty();
-            } else {
-                this.righty();
-            }
-        }
-    }
-
-    /**** Start Animating *****/
-
-    //Animating "Hero Jumbotron" section text.
-
-    const $introText = new SlidElement("#intro-text", R);
-    const $introSubText = new SlidElement("#intro-subtext", L);
-
     //Logic to slide elements on scrolling
 
     let $animation_elements = $('.anim');
