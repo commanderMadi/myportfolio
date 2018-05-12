9b11bdec3d7d7b866f7709363fbf2def880d2145
Darth Coder <00darthcoder00@gmail.com>
2018-05-12 09:13:14 +0200

    User let instead of var

--- public/js/custom.js
+++ public/js/custom.js
@@ -55,20 +55,21 @@ $(function () { //On Document Ready
     const $introText = new SlidElement("#intro-text", R);
     const $introSubText = new SlidElement("#intro-subtext", L);
 
+    //Logic to slide elements on scrolling
 
-    var $animation_elements = $('.anim');
-    var $window = $(window);
+    let $animation_elements = $('.anim');
+    let $window = $(window);
 
     function check_if_in_view() {
-        var window_height = $window.height();
-        var window_top_position = $window.scrollTop();
-        var window_bottom_position = (window_top_position + window_height);
+        let window_height = $window.height();
+        let window_top_position = $window.scrollTop();
+        let window_bottom_position = (window_top_position + window_height);
 
         $.each($animation_elements, function () {
-            var $element = $(this);
-            var element_height = $element.outerHeight();
-            var element_top_position = $element.offset().top;
-            var element_bottom_position = (element_top_position + element_height);
+            let $element = $(this);
+            let element_height = $element.outerHeight();
+            let element_top_position = $element.offset().top;
+            let element_bottom_position = (element_top_position + element_height);
 
             //check to see if this current container is within viewport
             if ((element_bottom_position >= window_top_position) &&
